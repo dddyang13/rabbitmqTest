b528ec3ac2f2ca1eb6caab2caebfcd380d540855
dddyang13 <zcyx13@sina.com>
2016-04-22 16:00:19 +0800

    Update RabbitMQMessageSender.java

--- src/main/java/com/crawler/docker_test/RabbitMQMessageSender.java
+++ src/main/java/com/crawler/docker_test/RabbitMQMessageSender.java
@@ -69,7 +69,7 @@ public class RabbitMQMessageSender implements MessageSender {
 
 	public static void main(String[] args) throws Exception {
 		//通过配置文件获取配置项
-		String ip=InetAddress.getByName(Config.getProperty("host")).getHostAddress();
+		String ip=Config.getProperty("host");
 		Integer port=Config.getIntProperty("port");
 		String user="guest";
 		String password="guest";
@@ -90,17 +90,18 @@ public class RabbitMQMessageSender implements MessageSender {
 		}
 		//通过系统环境变量获取配置项
 		if(System.getenv("rabbitmq_ip")!=null)
-			ip=InetAddress.getByName(System.getenv("rabbitmq_ip")).getHostAddress();
+			ip=System.getenv("rabbitmq_ip");
 		if(System.getenv("rabbitmq_port")!=null)
 			port=Integer.parseInt(System.getenv("rabbitmq_port"));
 		if(System.getenv("rabbitmq_user")!=null)
 			user=System.getenv("rabbitmq_user");
 		if(System.getenv("rabbitmq_password")!=null)
 			password=System.getenv("rabbitmq_password");
-		System.out.println("获取IP为："+ip);
-		String brokers =ip +":"+port;
+		System.out.println("获取ip为："+ip);
+		String brokers =InetAddress.getByName(ip).getHostAddress() +":"+port;
+		System.out.println("获取brokers为："+brokers);
 		
-//		String brokers="10.1.235.30:10001";
+//		brokers="10.1.235.30:10001";
 		String queue = "LocalCrawlTaskQueue";
 
 		MessageSender messageSender = new RabbitMQMessageSender(brokers, queue,user,password);
